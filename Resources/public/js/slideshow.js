dd40c9a6813f16ae7ddb50ec8070a84f7514d084
Sam Mateosian <sam@bigroomstudios.com>
2012-04-05 14:42:40 +0000

    added next/back page at the end of a page slideshow

--- Resources/public/js/slideshow.js
+++ Resources/public/js/slideshow.js
@@ -1,65 +1,12 @@
 
 var supports_history = ( window.history && history.pushState ? true : false ); 
 
-$('#slide-next').live('click', function(event){
-	
-	event.preventDefault();
-	
-	$j.widgets['swiper'].next();
-	
-	//updateSlideHash();
-	
-});
-
-$('#slide-next').live('touchstart', function(event){
-	
-	event.preventDefault();
-	
-	$j.widgets['swiper'].next();
-	
-	//updateSlideHash();
-	
-});
-
-$('.slide-next').live('click', function(event){
-	
-	//alert('click');
-	
-	event.preventDefault();
-	
-	$j.widgets['swiper'].next();
-	
-	//updateSlideHash();
-});
-
-$('.slide-next').live('touchstart', function(event){
-	
-	//alert('touchstart');
-	
-	event.preventDefault();
-	
-	$j.widgets['swiper'].next();
-	
-	//updateSlideHash();
-});
-
-$('#slide-back').live('click', function(event){
-	
-	event.preventDefault();
-	
-	$j.widgets['swiper'].prev();
-	
-	//updateSlideHash();
-});
-
-$('#slide-back').live('touchstart', function(event){
-	
-	event.preventDefault();
-	
-	$j.widgets['swiper'].prev();
-	
-	//updateSlideHash();
-});
+$('#slide-next').live('click', slideNext);
+$('#slide-next').live('touchstart', slideNext);
+$('.slide-next').live('click', slideNext);
+$('.slide-next').live('touchstart', slideNext);
+$('#slide-back').live('click', slideBack);
+$('#slide-back').live('touchstart', slideBack);
 
 
 $('#slide-indicators a').live('click', function(event){
@@ -75,13 +22,70 @@ $('#slide-indicators a').live('click', function(event){
 	//updateSlideHash();
 });
 
+function slideNext(event){
+	
+	event.preventDefault();
+	
+	var $target = $(event.target);
+	
+	if($target.hasClass('disabled')){
+		
+		alert('here');
+		
+		return false;
+	}
+	
+	var swiper = $j.widgets['swiper'];
+	
+	//console.log(swiper.index + ' ' + swiper.length);
+	
+	if (swiper.index < swiper.length - 1){
+		
+		swiper.next();
+		
+	}else{
+		
+		$j.page_nav.go_next();
+	}
+}
+
+function slideBack(event){
+	
+	event.preventDefault();
+	
+	var $target = $(event.target);
+	
+	if($target.hasClass('disabled')){
+		
+		alert('here');
+		
+		return false;
+	}
+
+	var swiper = $j.widgets['swiper'];
+	
+	//console.log(swiper.index + ' ' + swiper.length);
+	
+	if (swiper.index > 0){
+		
+		swiper.prev();
+		
+	}else{
+		
+		$j.page_nav.go_prev();
+	}
+}
+
+
 function updateSlideIndicators(){
 	
 	var index = 0;
 	
-	if($j.widgets['swiper']){
+	var swiper = $j.widgets['swiper'];
 	
-		index = $j.widgets['swiper'].index;
+	if(swiper){
+		
+		index = swiper.index;
 	}
 	
 	var $indicators = $('#slide-indicators a');
@@ -89,6 +93,28 @@ function updateSlideIndicators(){
 	$indicators.removeClass('selected');
 	
 	$($indicators[index]).addClass('selected');
+	
+	$back_link = $('#slide-back a');
+	
+	$next_link = $('#slide-next a');
+	
+	if(swiper && (swiper.index == 0) && $j.page_nav && (!$j.page_nav.get_prev_page())){
+		
+		$back_link.addClass('disabled');
+		
+	}else{
+			
+		$back_link.removeClass('disabled');
+	}
+	
+	if(swiper && (swiper.index == swiper.length - 1) && $j.page_nav && (!$j.page_nav.get_next_page())){
+		
+		$next_link.addClass('disabled');
+		
+	}else{
+			
+		$next_link.removeClass('disabled');
+	}
 }
 
 
